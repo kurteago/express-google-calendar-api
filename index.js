f28f6941a8f60df9fbc9117134175c888ec1b25f
kurteago <79864618+kurteago@users.noreply.github.com>
2021-03-01 14:22:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
