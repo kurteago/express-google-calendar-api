051275154c7e002394835e95126a0983d1002ef8
Tiago <kurtiago@gmail.com>
2021-03-20 14:15:09 -0300

    back to iury's way

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
 
-const { client_id, client_secret, redirect_uris, refresh_token } = process.env;
+const { client_id, client_secret, redirect_uris, refresh_token, GOOGLE_CREDENTIALS } = process.env;
 
 const express = require("express");
 const cors = require("cors");
@@ -60,13 +60,13 @@ app.post("/create-event", async (req, res) => {
 //get future event for given email: 'q: req.body.q'
 app.post("/get-events", async (req, res) => {
   try {
-    ///const oAuth2Client = new OAuth2(client_id, client_secret, redirect_uris);
+    const oAuth2Client = new OAuth2(client_id, client_secret, redirect_uris);
     
-    //oAuth2Client.setCredentials({ refresh_token });
-    const auth = new google.auth.GoogleAuth({
+    oAuth2Client.setCredentials({ refresh_token });
+    /* const auth = new google.auth.GoogleAuth({
       keyFile: 'credentials.json',
       scopes: ['https://www.googleapis.com/auth/calendar'],
-    });
+    }); */
     
     const result = await calendar.events.list({
       calendarId: req.body.calendarId,
@@ -76,7 +76,7 @@ app.post("/get-events", async (req, res) => {
       //maxResults: 10,
       singleEvents: true,
       orderBy: 'startTime',
-      auth: auth,
+      auth: oAuth2Client,
       //fields: 'items(attendees/email)'
     });
 
